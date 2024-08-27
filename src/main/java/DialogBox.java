40b63144b46c40bfe2f002abcb77e009eccc1ce0
Yong Khee Hou <yongkheehou@gmail.com>
2024-08-27 20:54:10 +0800

    Implement GUI up till tutorial 3
    Add images, test, and extra styling

---
+++ src/main/java/DialogBox.java
@@ -0,0 +1,51 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.shape.Circle;
+
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogBox(String s, Image i) {
+        text = new Label(s);
+        displayPicture = new ImageView(i);
+        Circle clip = new Circle(50, 50, 50); // Radius of 50, adjust as needed
+        displayPicture.setClip(clip);
+
+        this.getChildren().addAll(text, displayPicture);
+
+        //Styling the dialog box
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.setSpacing(10); // 10px spacing between ImageView and Label
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(String s, Image i) {
+        return new DialogBox(s, i);
+    }
+
+    public static DialogBox getDukeDialog(String s, Image i) {
+        var db = new DialogBox(s, i);
+        db.flip();
+        return db;
+    }
+}
