559776f3a11d4b20f43e654eb00fce881a26ed05
Yong Khee Hou <yongkheehou@gmail.com>
2024-08-30 07:55:14 +0800

    Add additional styling using CSS
    Created button.css, root.css, scrollpane.css, textfield.css, and
    vbox.css in resources/css to manage styling of components

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,14 @@ public class Main extends Application {
     public void start(Stage stage) {
         MainWindow mainWindow = new MainWindow(stage);
         mainWindow.setDuke(duke);
+
+        String rootCss = this.getClass().getResource("/css/root.css").toExternalForm();
+        String scrollPaneCss = this.getClass().getResource("/css/scrollpane.css").toExternalForm();
+        String vboxCss = this.getClass().getResource("/css/vbox.css").toExternalForm();
+        String textFieldCss = this.getClass().getResource("/css/textfield.css").toExternalForm();
+        String buttonCss = this.getClass().getResource("/css/button.css").toExternalForm();
+
+        mainWindow.getScene().getStylesheets().addAll(rootCss, scrollPaneCss, vboxCss, textFieldCss, buttonCss);
     }
 
     public static void main(String[] args) {
